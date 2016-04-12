7052eee3b8d5c871f666b371e8d2c7834004a959
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-12 13:48:50 -0400

    add front end form

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,27 @@ socket.on('connect', function(){
 	console.log('Connected to socket.io server!');
 });
 
+
+
 socket.on('message', function(message){
 	console.log('New message:');
 	console.log(message.text);
+	//var messageToHTML = '<h1>' + message.text + '</h1>';
+	//document.getElementById('received-message').innerHTML += messageToHTML;
+});
+
+
+
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event){
+	event.preventDefault(); //when you dont want to refresh the entire page
+
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val()
+	});
+
+	this.reset();
 });
